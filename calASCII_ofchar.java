37c389145ca4e7c87fb13ec678c6d5995fd74058
Pranil01 <94604003+Pranil01@users.noreply.github.com>
2023-06-23 23:01:42 +0530

    Add files via upload

---
+++ calASCII_ofchar.java
@@ -0,0 +1,13 @@
+package ClcuASCIIofChar;
+
+public class calASCII_ofchar {
+
+	public static void main(String[] args) {
+		 char c='A';
+		 int i = c;
+		 System.out.println("ASCII value of "+c+" ="+i);
+		 
+
+	}
+
+}
