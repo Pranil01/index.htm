39a54d229a3041a13f97d21859831bb78e3030f5
Pranil01 <94604003+Pranil01@users.noreply.github.com>
2023-06-23 23:00:45 +0530

    Add files via upload

---
+++ Alpha_or_not.java
@@ -0,0 +1,15 @@
+package Charisaplhaornot;
+
+public class Alpha_or_not {
+
+	public static void main(String[] args) {
+		char ch;
+		ch='a';
+		
+		if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')) {
+		System.out.println(ch + ":is Alphabet");}
+		else {
+			System.out.println(ch + ":is not Alphabet");}
+	}
+
+}
---
+++ Alpha_or_not1.java
@@ -0,0 +1,17 @@
+package Charisaplhaornot;
+
+public class Alpha_or_not1 {
+
+	public static void main(String[] args) {
+		char ch ='I';
+		
+		if((ch>=97&&ch<=122)||(ch>=65&&ch<=90)) {
+			System.out.println(ch +": is Alphabet");
+		}
+		else {
+			System.out.println(ch+":is not Alphabet");
+		}
+
+	}
+
+}
